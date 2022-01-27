82d68dec17447ad6d35bcbd67e632b3ae4e991d1
AnikaDev <anna.kimlaeva@gmail.com>
2022-01-27 19:51:13 +0300

    init

---
+++ Практика/test1.py
@@ -0,0 +1,5 @@
+a = 'white   cat        black  cat'
+print(a.split(' '))  # разбивает строку на список текстовых элементов по разделителю
+print(a.split())
+print(a.split('cat'))
+print('***'.join(['white', 'cat', 'black', 'cat']))  #
